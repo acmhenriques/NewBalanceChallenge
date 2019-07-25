48727a1720baf6c876ae5a2af3c8b2b2b753ba52
Ana Catarina Marques Henriques <acmhenriques@MBP-de-Ana.home>
2019-07-25 20:03:53 +0100

    alt

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var tl = new TimelineMax();
 tl.from(".btn-sneakers3", 5, {y: -40, opacity:0, ease: Power4.easeOut});
 
 var tl = new TimelineMax();
-tl.from(".header p", 0.5, {opacity:0, ease: Power0.easeOut});
-tl.from(".header p", 0.5, {opacity:1, ease: Power0.easeOut});
-tl.from(".header p", 0.5, {opacity:0, ease: Power0.easeOut});
-tl.from(".header p", 0.5, {opacity:1, ease: Power0.easeOut});
-tl.from(".header p", 0.5, {opacity:0, ease: Power0.easeOut});
+tl.from(".new", 0.5, {opacity:0, ease: Power0.easeOut});
+tl.from(".new", 0.5, {opacity:1, ease: Power0.easeOut});
+tl.from(".new", 0.5, {opacity:0, ease: Power0.easeOut});
+tl.from(".new", 0.5, {opacity:1, ease: Power0.easeOut});
+tl.from(".new", 0.5, {opacity:0, ease: Power0.easeOut});
