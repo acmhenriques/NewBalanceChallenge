2b582b7f8fc5da7ff98100ad0ab0e0d464e82f37
Ana Catarina Marques Henriques <acmhenriques@MBP-de-Ana.home>
2019-07-25 18:44:25 +0100

    alt

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var tl = new TimelineMax();
 tl.from("h1", 5, {y: -40, opacity:0, ease: Power4.easeOut});
+
+var tl = new TimelineMax();
+tl.from(".btn-sneakers1", 5, {y: -40, opacity:0, ease: Power4.easeOut});
+
+var tl = new TimelineMax();
+tl.from(".btn-sneakers2", 5, {y: -40, opacity:0, ease: Power4.easeOut});
+
+var tl = new TimelineMax();
+tl.from(".btn-sneakers3", 5, {y: -40, opacity:0, ease: Power4.easeOut});
